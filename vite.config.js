b88a63132b97bf7bca9a1d65cd14225331d62735
lokeshbhaskar <lokeshbhaskar28@gmail.com>
2025-02-03 11:57:45 +0530

    forth

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,29 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(),
+//     tailwindcss(),
+//   ],
+// })
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // Moves dependencies to a separate chunk
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Optional: Increase warning limit (temporary fix)
+  },
 })
